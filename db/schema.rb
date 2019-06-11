e717f5d48454ca2c6ccc989e5e10c3a19e93df34
Charlie Depman <cdepman@gmail.com>
2019-06-10 23:29:16 -0700

    associate user and link ource 'https://rubygems.org'

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_062250) do
+ActiveRecord::Schema.define(version: 2019_06_11_062809) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
